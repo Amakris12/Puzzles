422f36bb9e2c81fe69a45384fdbece9d8c4e6004
Amakris12 <amakri386@west-mec.org>
2021-11-02 15:21:51 -0700

    final

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const server = http.createServer(function (req,res){
-    if(req.url === '/'){
-        res.send('<p>Home.html<p>')
-    }
-    if(req.url === '/one'){
-        
-    }
-    if(req.url === '/two'){
-        
-    }
-    if(req.url === '/three'){
-        
-    }
-    if(req.url === '/four'){
-       
-    }
-    if(req.url === '/five'){
-        
-    }
-    if(req.url === '/six'){
-        
-    }
+app.get('/',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/Home.html'))
 })
-
-server.listen(5000)
+app.get('/index.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles', '/index.css'))
+})
+app.get('/first',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/pages','puzzleOneQuiz.html'))
+})
+app.get('/quiz.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/quiz.css'))
+})
+app.get('/ssss',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/pages','puzzleTwoTrivia.html'))
+})
+app.get('/trivia.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/trivia.css'))
+})
+app.get('/nextOne',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/Pages','puzzleThreeRiddle.html'))
+})
+app.get('/riddle.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/riddle.css'))
+})
+app.get('/moveAlongPeople',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/Pages','puzzleFourFillin.html'))
+})
+app.get('/fillin.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/fillin.css'))
+})
+app.get('/southPark',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/Pages','puzzleFiveMath.html'))
+})
+app.get('/math.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/math.css'))
+})
+app.get('/moveOn',(req,res)=>{
+    console.log('user hit the resourse')
+    res.status(200).sendFile(path.join(__dirname+'/Pages','puzzleSixHunt.html'))
+})
+app.get('/hunt.css', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.join(__dirname, '../Puzzles', '/styles','/pages','/hunt.css'))
+})
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000....')
+})
